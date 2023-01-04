135c98689b4ae53c28a6f432c18c8b0065d14cb8
marridG <marridG@sjtu.edu.cn>
2023-01-04 16:25:45 +0800

    init obstacle-detect pipeline [SUBJECT] - [TESTED] NO (nothing to test) - [ADDED] obstacle_detect.py - [EDITED] - [RMVED]

---
+++ Final Project Railway Track Detection/obstacle_detect.py
@@ -0,0 +1,12 @@
+import os
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+import video_handler
+import obj_detect
+
+
+class ObstacleDetect:
+    def __init__(self):
+        pass
